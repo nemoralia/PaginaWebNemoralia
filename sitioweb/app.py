03731d95159276d221951110abd985f8bd39d84f
Nemoralia <f3nriblack@gmail.com>
2024-02-01 21:32:05 -0500

    se realiza cambios en la pagina, pendiente corregir el modulo de libros

--- sitioweb/app.py
+++ sitioweb/app.py
@@ -15,13 +15,17 @@ def libros():
 def nosotros():
     return render_template('sitio/nosotros.html')
 
-@app.route('/admin')
+@app.route('/admin/')
 def admin_index():
     return render_template('admin/index.html')
 
 @app.route('/admin/login')
 def admin_login():
     return render_template('admin/login.html')
+
+@app.route('/admin/libros')
+def admin_libros():
+    return render_template('admin/libros.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
